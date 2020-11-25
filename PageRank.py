f6287b148e6ece4ce63a2490432103c17d36b78e
Junda Lou <loudakkk@hotmail.com>
2020-11-24 16:30:56 -0800

    commit 2

--- PageRank.py
+++ PageRank.py
@@ -3,30 +3,36 @@ import math
 
 def pageRank():
     outbounds, inbounds = PageRankPreProcess.calculate_all_pages('outlink_maps')
+    # print(inbounds, outbounds)
 
     init_rank = 1 / len(outbounds)
     ranks = {node : init_rank for node in outbounds.keys()}
     
     while True:
+        # print(sorted(ranks.items(), key=lambda item: item[1], reverse=True))
         next_ranks = {}
         for node in outbounds.keys():
             rank = 0
             for inbound in inbounds[node]:
                 rank += ranks[inbound] / outbounds[inbound]
+            rank = rank * (1 - 0.2) + (0.2 / len(outbounds))
             next_ranks[node] = rank
-        
-        isclose = True
-        for key in ranks.keys():
-            isclose &= math.isclose(ranks[key], next_ranks[key], abs_tol=1e-15)
 
-        if isclose:
+        if ranks == next_ranks:
             break
 
         ranks = next_ranks
+        
 
     ranks = sorted(ranks.items(), key=lambda item: item[1], reverse=True)
-    
+ 
     print(ranks)
+
+    sum = 0
+    for v in ranks:
+        sum += v[1]
+    print(sum)
+
     return ranks
 
 if __name__ == '__main__':
